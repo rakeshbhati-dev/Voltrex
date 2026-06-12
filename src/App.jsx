ed4c02ac8179c5ec3a2e50e2807cc30b31ff49f8
rakeshbhati-dev <rakeshbhati.dev@gmail.com>
2026-06-12 16:48:55 +0530

    Add hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Navbar from './components/Navbar'
+import Hero from './sections/Hero'
+import Programs from './sections/Programs'
 
 function App() {
   return (
-    <div>
+    <div className='background-dark text-sans leading-normal'>
       <Navbar />
+      <Hero />
+      <Programs />
     </div>
   )
 }
