6aa1d934d2534f1458719ec86620663744b0051c
rakeshbhati-dev <rakeshbhati.dev@gmail.com>
2026-06-12 11:30:32 +0530

    Create navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react'
+import Logo from './Logo'
+import Button from './Button'
+import { Menu, X } from 'lucide-react'
+
+function Navbar() {
+    const [isOpen, setIsOpen] = useState(false)
+    return (
+        <nav className='bg-dark/95 border-b border-border sticky top-0 z-10'>
+            <div className='flex justify-between items-center py-4 px-6 md:px-10'>
+                <Logo />
+                <div className='hidden md:flex text-gray gap-6 font-semibold text-sm tracking-wider'>
+                    <a href="" className='hover:text-primaryDark'>PROGRAMS</a>
+                    <a href="" className='hover:text-primaryDark'>TRAINERS</a>
+                    <a href="" className='hover:text-primaryDark'>PRICING</a>
+                    <a href="" className='hover:text-primaryDark'>GALLERY</a>
+                    <a href="" className='hover:text-primaryDark'>ABOUT</a>
+                </div>
+                <Button title='JOIN NOW' buttonStyle='hidden md:block' />
+                <button className='md:hidden text-primary'
+                    onClick={() => setIsOpen((prev) => !prev)}
+                >
+                    {isOpen ? <X size={24} /> : <Menu size={24} />}
+                </button>
+            </div>
+            {isOpen &&
+            <div className='md:hidden bg-dark/95 border-t border-border px-6 py-4 flex flex-col gap-4 text-xs text-gray font-semibold tracking-wider'>
+                <a href="" className='hover:text-primary' onClick={()=>setIsOpen((prev)=>!prev)}>PROGRAMS</a>
+                <a href="" className='hover:text-primary' onClick={()=>setIsOpen((prev)=>!prev)}>TRAINERS</a>
+                <a href="" className='hover:text-primary' onClick={()=>setIsOpen((prev)=>!prev)}>PRICING</a>
+                <a href="" className='hover:text-primary' onClick={()=>setIsOpen((prev)=>!prev)}>GALLERY</a>
+                <a href="" className='hover:text-primary' onClick={()=>setIsOpen((prev)=>!prev)}>ABOUT</a>
+            </div>
+            }
+        </nav>
+    )
+}
+
+export default Navbar
