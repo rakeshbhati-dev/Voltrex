5e2977800ed1b634935221bbc7ce7a1a1e59440b
rakeshbhati-dev <rakeshbhati.dev@gmail.com>
2026-06-12 16:51:20 +0530

    Create programs section

---
+++ src/sections/Programs.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Section from '../components/Section'
+
+function Programs() {
+  return (
+    <Section>
+      
+    </Section>
+  )
+}
+
+export default Programs
