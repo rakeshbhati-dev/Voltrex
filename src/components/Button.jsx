6ec5d609e3750f91dc29184e914462e572239986
rakeshbhati-dev <rakeshbhati.dev@gmail.com>
2026-06-13 11:38:46 +0530

    fix text-color

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,12 +3,12 @@ import React from 'react'
 function Button({
     title,
     onClick,
-    buttonStyle
+    buttonStyle='text-white'
 }) {
   return (
     <button
     onClick={onClick}
-    className={` font-bold text-white bg-primary py-2 px-6 tracking-widest text-sm uppercase ${buttonStyle}`}
+    className={` font-bold bg-primary py-2 px-6 tracking-widest text-sm uppercase ${buttonStyle}`}
     >{title}</button>
   )
 }
